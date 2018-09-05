b7ea6147927c020ed4e4be726b315e16c80beb04
Isabella Chen <miss.izzyc@gmail.com>
2018-09-05 17:57:46 +0200

    added data file

---
+++ data/index.js
@@ -0,0 +1,8 @@
+const starships = require('./starships')
+const characters = require('./characters')
+const episodes = require('./episodes')
+module.exports = {
+  starships,
+  characters,
+  episodes
+}
